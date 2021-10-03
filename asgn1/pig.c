1563b804c901a6fd948f0ec26f0f653d531d91b4
bnguy118 <bnguy118@ucsc.edu>
2021-10-03 08:35:15 +0000

    Fixed pig.c

--- asgn1/pig.c
+++ asgn1/pig.c
@@ -22,12 +22,11 @@ int main(void) {
     int iseed;
     printf("Random seed: ");
     int seedcheck = scanf("%d", &iseed);
-    if (seedcheck == 1) {
-        srand(iseed);
-    }
     if (seedcheck != 1) {
         srand(SEED);
         fprintf(stderr, "Invalid random seed. Using 2021 instead.");
+    } else {
+        srand(iseed);
     }
 
     //makes points array holding each player's points starting with 0s
@@ -43,7 +42,7 @@ int main(void) {
     int sameplayer = 0;
     int hard = 0;
 
-    printf("%s rolls the pig... ", names[currentplayer]);
+    printf("%s rolls the pig...", names[currentplayer]);
     //Keeps playing until someone reaches one hundred points
     while (points[currentplayer] < 100) {
         //Rolls from zero to six
@@ -51,31 +50,31 @@ int main(void) {
 
         //Adds points or forfeits turns based on roll
         if (sameplayer == 0 && hard == 1) {
-            printf("\n%s rolls the pig... ", names[currentplayer]);
+            printf("\n%s rolls the pig...", names[currentplayer]);
         }
         if (pig[roll] == SIDE) {
-            printf("pig lands on side ");
+            printf(" pig lands on side");
             currentplayer++;
             sameplayer = 0;
             currentplayer = currentplayer % totalplayers;
         }
         if (pig[roll] == RAZORBACK) {
-            printf("pig lands on back ");
+            printf(" pig lands on back");
             points[currentplayer] += 10;
             sameplayer = 1;
         }
         if (pig[roll] == TROTTER) {
-            printf("pig lands upright ");
+            printf(" pig lands upright");
             points[currentplayer] += 10;
             sameplayer = 1;
         }
         if (pig[roll] == SNOUTER) {
-            printf("pig lands on snout ");
+            printf(" pig lands on snout");
             points[currentplayer] += 15;
             sameplayer = 1;
         }
         if (pig[roll] == JOWLER) {
-            printf("pig lands on ear ");
+            printf(" pig lands on ear");
             points[currentplayer] += 5;
             sameplayer = 1;
         }
