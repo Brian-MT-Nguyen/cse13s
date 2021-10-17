f70f62329898c29abd0a0426720b6d5c8ac7c012
bnguy118 <bnguy118@ucsc.edu>
2021-10-17 06:39:22 +0000

    Formmated files

--- asgn3/quick.c
+++ asgn3/quick.c
@@ -1,28 +1,29 @@
 #include "quick.h"
+
 #include "stats.h"
 
 #include <stdio.h>
 
 uint32_t partition(Stats *stats, uint32_t *A, uint32_t lo, uint32_t hi) {
-	uint32_t i = lo - 1;
-	for(uint32_t j = lo; j < hi; j++) {
-		if (cmp(stats, A[j - 1], A[i - 1]) == -1) {
-			i += 1;
-			swap(stats, &A[i - 1], &A[j - 1]);
-		}
-	}
-	swap(stats, &A[i], &A[hi - 1]);
-	return i + 1;
+    uint32_t i = lo - 1;
+    for (uint32_t j = lo; j < hi; j++) {
+        if (cmp(stats, A[j - 1], A[i - 1]) == -1) {
+            i += 1;
+            swap(stats, &A[i - 1], &A[j - 1]);
+        }
+    }
+    swap(stats, &A[i], &A[hi - 1]);
+    return i + 1;
 }
 
 void quick_sorter(Stats *stats, uint32_t *A, uint32_t lo, uint32_t hi) {
-	if (lo < hi) {
-		uint32_t p = partition(stats, A, lo, hi);	       
-		quick_sorter(stats, A, lo, p - 1);
-		quick_sorter(stats, A, p + 1, hi);
-	}
+    if (lo < hi) {
+        uint32_t p = partition(stats, A, lo, hi);
+        quick_sorter(stats, A, lo, p - 1);
+        quick_sorter(stats, A, p + 1, hi);
+    }
 }
 
 void quick_sort(Stats *stats, uint32_t *A, uint32_t n) {
-	quick_sorter(stats, A, 1, n);
+    quick_sorter(stats, A, 1, n);
 }
--- asgn3/sorting.c
+++ asgn3/sorting.c
@@ -158,7 +158,6 @@ int main(int argc, char **argv) {
             }
         }
         printf("\n");
-
     }
     free(A);
 }
