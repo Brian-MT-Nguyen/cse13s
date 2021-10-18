1e9a43217dc72ffde2e9957a0bc03f4e5b0f801f
bnguy118 <bnguy118@ucsc.edu>
2021-10-18 02:33:54 +0000

    Updating quick.c

--- asgn3/quick.c
+++ asgn3/quick.c
@@ -5,15 +5,15 @@
 #include <stdio.h>
 
 uint32_t partition(Stats *stats, uint32_t *A, uint32_t lo, uint32_t hi) {
-    uint32_t i = lo - 1;
-    for (uint32_t j = lo; j < hi; j++) {
-        if (cmp(stats, A[j - 1], A[hi - 1]) == -1) {
-            i += 1;
-            swap(stats, &A[i - 1], &A[j - 1]);
+    uint32_t lo_pointer  = lo - 1;
+    for (uint32_t hi_pointer = lo; hi_pointer < hi; hi_pointer++) {
+        if (cmp(stats, A[hi_pointer - 1], A[hi - 1]) == -1) {
+            lo_pointer += 1;
+            swap(stats, &A[lo_pointer - 1], &A[hi_pointer - 1]);
         }
     }
-    swap(stats, &A[i], &A[hi - 1]);
-    return i + 1;
+    swap(stats, &A[lo_pointer], &A[hi - 1]);
+    return lo_pointer + 1;
 }
 
 void quick_sorter(Stats *stats, uint32_t *A, uint32_t lo, uint32_t hi) {
