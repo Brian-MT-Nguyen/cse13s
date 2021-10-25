03d252f18e90961c1fcf623b4a38ebaba4e40cba
bnguy118 <bnguy118@ucsc.edu>
2021-10-25 02:40:35 +0000

    updating files

--- asgn4/stack.c
+++ asgn4/stack.c
@@ -62,8 +62,8 @@ bool stack_push(Stack *s, uint32_t x) {
 
 bool stack_pop(Stack *s, uint32_t *x) {
     if (stack_empty(s) == false) {
-        *x = s->items[s->top];
         s->top -= 1;
+        *x = s->items[s->top];
         return true;
     }
     return false;
