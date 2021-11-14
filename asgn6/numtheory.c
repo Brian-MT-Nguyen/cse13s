ce912d0894993ec0f78c3036402a14d2b03d62fb
Brian Nguyen <bnguy118@ucsc.edu>
2021-11-14 09:51:35 +0000

    updating numtheory

--- asgn6/numtheory.c
+++ asgn6/numtheory.c
@@ -44,7 +44,7 @@ void mod_inverse(mpz_t i, mpz_t a, mpz_t n) {
     //Initialize temp variable to fake parrallel assignment
     mpz_t temp;
     mpz_init(temp);
-    while (mpz_sgn(r) != 0) {
+    while (mpz_sgn(r_prime) != 0) {
         mpz_fdiv_q(q, r, r_prime);
 
         mpz_set(temp, r);
@@ -66,7 +66,7 @@ void mod_inverse(mpz_t i, mpz_t a, mpz_t n) {
         mpz_add(i, i, n);
     }
 
-    mpz_clears(r, r_prime, i_prime, q, temp);
+    mpz_clears(r, r_prime, i_prime, q, temp, NULL);
 }
 
 //Computes base^exponent mod modulus, then puts value in out
@@ -76,44 +76,49 @@ void mod_inverse(mpz_t i, mpz_t a, mpz_t n) {
 //exponent: number that is the exponent of the base
 //modulus: number that is be used to mod value
 void pow_mod(mpz_t out, mpz_t base, mpz_t exponent, mpz_t modulus) {
-    //Initialize variable that holds base which will change in function
-    mpz_t changing_base;
-
-    //Initialize variable to check if exponent is odd
-    mpz_t odd_checker;
-    mpz_init(odd_checker);
+    //Initialize variable that holds base & exponent which will change in function
+    mpz_t changing_base, changing_exponent;
 
     mpz_set_ui(out, 1);
     mpz_init_set(changing_base, base);
+    mpz_init_set(changing_exponent, exponent);
 
-    while (mpz_sgn(exponent) == 1) {
-        if (mpz_mod_ui(odd_checker, exponent, 2) == 1) {
+    while (mpz_sgn(changing_exponent) == 1) {
+        if (mpz_odd_p(changing_exponent) != 0) {
             mpz_mul(out, out, changing_base);
             mpz_mod(out, out, modulus);
         }
         mpz_mul(changing_base, changing_base, changing_base);
         mpz_mod(changing_base, changing_base, modulus);
-        mpz_fdiv_q_ui(exponent, exponent, 2);
+        mpz_fdiv_q_ui(changing_exponent, changing_exponent, 2);
     }
-    mpz_clears(changing_base, odd_checker);
+    mpz_clears(changing_base, changing_exponent, NULL);
 }
 //Tests if a number is prime via the Miller-Rabin Algorithm
 //
 //n: the number to test if prime or not
 //iters: amount of iterations to test prime number with
 bool is_prime(mpz_t n, uint64_t iters) {
-    //Initialize  s and r from n
-    mpz_t s, r;
-    mpz_inits(s, r);
-    mpz_sub_ui(r, n, 1);
-
-    //Initialize modulus checker to see if n is even
-    mpz_t mod_checker;
-    mpz_init(mod_checker);
-    //Check if n is even or two
-    if ((mpz_cmp_ui(n, 2) == 0) || mpz_mod_ui(mod_checker, n, 2) == 0) {
+    //Check if n is even
+    if (mpz_even_p(n) != 0) {
+        //Check if n is two
+        if (mpz_cmp_ui(n, 2) == 0) {
+            return true;
+        }
+        //Else its not prime
         return false;
     }
+
+    //Check if n is three(prime) to prevent impossible calculations
+    if (mpz_cmp_ui(n, 3) == 0) {
+        return true;
+    }
+
+    //Initialize  s and r from n
+    mpz_t s, r;
+    mpz_inits(s, r, NULL);
+    mpz_sub_ui(r, n, 1);
+
     //Divides r until r is odd and increments s, satisfying equation from pseudo
     while (mpz_even_p(r) != 0) {
         mpz_fdiv_q_ui(r, r, 2);
@@ -122,34 +127,40 @@ bool is_prime(mpz_t n, uint64_t iters) {
 
     //Initialize variables that generates random a in range two to n-two
     mpz_t range, a;
-    mpz_inits(range, a);
+    mpz_inits(range, a, NULL);
     mpz_sub_ui(range, n, 3);
 
     //Initialize variables used in algorithm
-    mpz_t y, j, two, conditional_n, conditional_s;
-    mpz_inits(y, j, two, conditional_n, conditional_s);
+    mpz_t y, y_temp, j, two, conditional_n, conditional_s;
+    mpz_inits(y, y_temp, j, two, conditional_n, conditional_s, NULL);
     mpz_sub_ui(conditional_n, n, 1);
     mpz_sub_ui(conditional_s, s, 1);
     mpz_set_ui(two, 2);
-    for (uint64_t i = 0; i <= iters; i++) {
+    for (uint64_t i = 1; i <= iters; i++) {
         mpz_urandomm(a, state, range);
         mpz_add_ui(a, a, 2);
         pow_mod(y, a, r, n);
         if ((mpz_cmp_ui(y, 1) != 0) && (mpz_cmp(y, conditional_n) != 0)) {
             mpz_set_ui(j, 1);
             while ((mpz_cmp(j, conditional_s) <= 0) && (mpz_cmp(y, conditional_n) != 0)) {
-                pow_mod(y, y, two, n);
+                mpz_set(y_temp, y);
+                pow_mod(y, y_temp, two, n);
+
+                if (mpz_cmp_ui(y, 1) == 0) {
+                    mpz_clears(
+                        s, r, range, a, y, y_temp, j, two, conditional_n, conditional_s, NULL);
+                    return false;
+                }
+                mpz_add_ui(j, j, 1);
             }
-            if (mpz_cmp_ui(y, 1) == 0) {
-                return false;
-            }
-            mpz_add_ui(j, j, 1);
             if (mpz_cmp(y, conditional_n) != 0) {
+                mpz_clears(s, r, range, a, y, y_temp, j, two, conditional_n, conditional_s, NULL);
                 return false;
             }
         }
     }
-    mpz_clears(s, r, a, mod_checker, range, y, j, two, conditional_n, conditional_s);
+
+    mpz_clears(s, r, range, a, y, y_temp, j, two, conditional_n, conditional_s, NULL);
     return true;
 }
 //Generates a number that is bits long, then tests if prime using is_prime
@@ -159,11 +170,11 @@ bool is_prime(mpz_t n, uint64_t iters) {
 //iters: the amount of iterations to test if a number is prime
 void make_prime(mpz_t p, uint64_t bits, uint64_t iters) {
     //Make sure p isn't prime to generate new one
-    mpz_set_ui(p, 4);
+    mpz_urandomb(p, state, bits);
 
     while (!is_prime(p, iters)) {
-        mpz_rrandomb(p, state, bits);
-        if (is_prime(p, iters)) {
+        mpz_urandomb(p, state, bits);
+        if ((is_prime(p, iters)) && (mpz_sizeinbase(p, 2) == bits)) {
             break;
         }
     }
