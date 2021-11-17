7bf7b754c160df819ccd9c197422c6ebdd42155c
Brian Nguyen <bnguy118@ucsc.edu>
2021-11-17 09:41:26 +0000

    updating comments for rsa

--- asgn6/rsa.c
+++ asgn6/rsa.c
@@ -130,13 +130,13 @@ void rsa_decrypt_file(FILE *infile, FILE *outfile, mpz_t n, mpz_t d) {
     mpz_t c;
     mpz_init(c);
 
-    //Reads from binary file starting index one to k-one (j is return value of total bits written)
+    //Reads from binary file starting index one to k-one (j is store variable of total bits written from export)
+    size_t j;
     while (gmp_fscanf(infile, "%Zx\n", c) > 0) {
         //Decrypt each scan from c
         rsa_decrypt(c, c, d, n);
 
         //Convert the bytes read from c to block and puts bytes read in j
-        size_t j;
         mpz_export(block, &j, 1, sizeof(uint8_t), 1, 0, c);
 
         //Write from block one to j - one to outfile
