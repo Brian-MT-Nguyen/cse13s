d0eae97e858ca85036e92a599e40d2bde173229b
Brian Nguyen <bnguy118@ucsc.edu>
2021-11-17 03:48:38 +0000

    updating rsa + encrypt

--- asgn6/encrypt.c
+++ asgn6/encrypt.c
@@ -1,5 +1,90 @@
+#include "randstate.h"
+#include "numtheory.h"
+#include "rsa.h"
 #include <stdio.h>
+#include <gmp.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <time.h>
 
-int main(void) {
-    printf("Hello");
+#define OPTIONS "i:o:n:vh"
+
+int main(int argc, char **argv) {
+    //Intializes input, output, and key files that will be grabbed for program
+    FILE *infile = stdin;
+    FILE *outfile = stdout;
+    FILE *pbfile;
+    //Initialize variables used to parse through command line
+    int opt = 0;
+    bool pb_opened = false;
+    bool verbose = false;
+    bool help = false;
+
+    //Parses through command line input options
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+        switch (opt) {
+        case 'i': infile = fopen(optarg, "r"); break;
+        case 'o': outfile = fopen(optarg, "w"); break;
+        case 'n': pbfile = fopen(optarg, "w"); break;
+        case 'v': verbose = true; break;
+        case 'h': help = true; break;
+        default: break;
+        }
+    }
+
+    //Prints help message if prompted
+    if (help) {
+        printf("SYNOPSIS\n");
+        printf("  Generates an RSA public/private key pair.\n");
+
+        printf("USAGE\n");
+        printf("  ./keygen [-hv] [-b bits] -i iters -n pbfile -d pvfile \n");
+
+        printf("OPTIONS\n");
+        printf("  -h             Display program usage and help.\n");
+        printf("  -v             Display verbose output program.\n");
+        printf("  -b bits        Minimum bits needed for public key n.\n");
+        printf("  -i iters       Miller-Rabin iterations for testing primes (default: 50).\n");
+        printf("  -n pbfile      Public key file (default: rsa.pub).\n");
+        printf("  -d pvfile      Private key file (default: rsa. priv).\n");
+        printf("  -s seed        Random seed for testing.\n");
+    }
+
+    //Creates/opens default write files if pbfile and/or pvfile was not specified
+    if (!pb_opened) {
+        pbfile = fopen("rsa.pub", "r");
+    }
+
+    //Initialize vars that will store public key info
+    mpz_t n, exponent, signature;
+    mpz_inits(n, exponent, signature, NULL);
+    char username[32];
+    rsa_read_pub(n, exponent, signature, username, pbfile);
+
+    if (verbose) {
+        gmp_printf("user = %s\n", username);
+        gmp_printf("s (%zu bits) = %Zd\n", mpz_sizeinbase(signature, 2), signature);
+        gmp_printf("n (%zu bits) = %Zd\n", mpz_sizeinbase(n, 2), n);
+        gmp_printf("e (%zu bits) = %Zd\n", mpz_sizeinbase(exponent, 2), exponent);
+    }
+
+    //Verify the signature
+    mpz_t verify_user;
+    mpz_init(verify_user);
+    mpz_set_str(verify_user, username, 62);
+    if (!rsa_verify(verify_user, signature, exponent, n)) {
+        fprintf(stderr, "Signature could not be verified. Exiting Program...");
+        exit(0);
+    }
+
+    rsa_encrypt_file(infile, outfile, n, exponent);
+
+    fclose(pbfile);
+    fclose(infile);
+    fclose(outfile);
+    mpz_clears(n, exponent, signature, verify_user, NULL);
 }
--- asgn6/rsa.c
+++ asgn6/rsa.c
@@ -90,13 +90,66 @@ void rsa_encrypt(mpz_t c, mpz_t m, mpz_t e, mpz_t n) {
     pow_mod(c, m, e, n);
 }
 
-void rsa_encrypt_file(FILE *infile, FILE *outfile, mpz_t n, mpz_t e);
+void rsa_encrypt_file(FILE *infile, FILE *outfile, mpz_t n, mpz_t e) {
+    //Set k to block size that will be read
+    size_t k = (mpz_sizeinbase(n, 2) - 1) / 8;
+    uint8_t *block = (uint8_t *) calloc(k, sizeof(uint8_t));
+
+    //Prepend padding at byte zero to avoid impossible cases
+    block[0] = 0xFF;
+
+    //Initialize m which will store the data of the read file
+    mpz_t m;
+    mpz_init(m);
+
+    //Reads from binary file starting index one to k-one (j is return value of total bits written)
+    size_t j;
+    while ((j = fread(block + 1, sizeof(uint8_t), k - 1, infile) > 0)) {
+        //Convert the bytes read from file into m (including the 0th byte)
+        mpz_import(m, j + 1, 1, sizeof(uint8_t), 1, 0, block);
+
+        //Encrypt the read message and write it to outfile
+        rsa_encrypt(m, m, e, n);
+        gmp_fprintf(outfile, "%Zx\n", m);
+    }
+    //Free/Clear memory
+    free(&block);
+    mpz_clear(m);
+}
 
 void rsa_decrypt(mpz_t m, mpz_t c, mpz_t d, mpz_t n) {
     pow_mod(m, c, d, n);
 }
 
-void rsa_decrypt_file(FILE *infile, FILE *outfile, mpz_t n, mpz_t d);
+void rsa_decrypt_file(FILE *infile, FILE *outfile, mpz_t n, mpz_t d) {
+    //Set k to block size that will be read
+    size_t k = (mpz_sizeinbase(n, 2) - 1) / 8;
+    uint8_t *block = (uint8_t *) calloc(k, sizeof(uint8_t));
+
+    //Prepend padding at byte zero to avoid impossible cases
+    block[0] = 0xFF;
+
+    //Initialize c which stores scanned lines from infile
+    mpz_t c;
+    mpz_init(c);
+
+    //Reads from binary file starting index one to k-one (j is return value of total bits written)
+    size_t j;
+    while ((j = gmp_fscanf(infile, "%Zx\n", c) > 0)) {
+        //Decrypt each scan from c
+        rsa_decrypt(c, c, d, n);
+
+        //Convert the bytes read from c to block
+        mpz_export(block, &j, 1, sizeof(uint8_t), 1, 0, c);
+
+        //Write from block index to outfile excluding index zero
+        fwrite(block + 1, sizeof(uint8_t), j - 1, outfile);
+    }
+
+    //Free/Clear memory
+    free(&block);
+    mpz_clear(c);
+}
 
 void rsa_sign(mpz_t s, mpz_t m, mpz_t d, mpz_t n) {
     pow_mod(s, m, d, n);
