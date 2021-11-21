93e3e667ba5cc3a1dfc57b8073b02a30c4ec9892
Brian Nguyen <bnguy118@ucsc.edu>
2021-11-21 05:05:13 +0000

    updating include files of programs

--- asgn6/encrypt.c
+++ asgn6/encrypt.c
@@ -1,10 +1,7 @@
 #include "rsa.h"
 #include <stdio.h>
 #include <gmp.h>
-#include <inttypes.h>
-#include <stdbool.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <time.h>
