ba29beb4b3ef12a73ca6af3237a1cadc944e608f
Brian Nguyen <bnguy118@ucsc.edu>
2021-11-30 07:37:38 +0000

    added untested bst

---
+++ asgn7/bst.c
@@ -0,0 +1,74 @@
+#include "bst.h"
+#include <stdio.h>
+#include <string.h>
+//
+Node *bst_create(void) {
+    Node *root = node_create(NULL, NULL);
+    return root;
+}
+
+void bst_delete(Node **root) {
+    if ((*root)->left && (*root)->right && *root) {
+        bst_delete(&(*root)->left);
+        bst_delete(&(*root)->right);
+        node_delete(&*root);
+    }
+}
+
+//Helper function to help compare and find the maximum of 2 values
+static int max(int x, int y) {
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+//
+uint32_t bst_height(Node *root) {
+    if (root) {
+        return 1 + max(bst_height(root->left), bst_height(root->right));
+    }
+    return 0;
+}
+
+uint32_t bst_size(Node *root) {
+    if (root) {
+        return bst_size(root->left) + 1 + bst_size(root->right);
+    }
+    return 0;
+}
+
+Node *bst_find(Node *root, char *oldspeak) {
+    if (root) {
+        if (strcmp(root->oldspeak, oldspeak) > 0) {
+            return bst_find(root->left, oldspeak);
+        } else if (strcmp(root->oldspeak, oldspeak) < 0) {
+            return bst_find(root->right, oldspeak);
+        }
+        return root;
+    }
+    return root;
+}
+
+Node *bst_insert(Node *root, char *oldspeak, char *newspeak) {
+    if (root) {
+        if (strcmp(root->oldspeak, oldspeak) == 0) {
+            return 0;
+        } else if (strcmp(root->oldspeak, oldspeak) > 0) {
+            root->left = bst_insert(root->left, oldspeak, newspeak);
+        } else if (strcmp(root->oldspeak, oldspeak) < 0) {
+            root->right = bst_insert(root->right, oldspeak, newspeak);
+        }
+        return root;
+    }
+    return root;
+}
+
+void bst_print(Node *root) {
+    if (root->left && root->right && root) {
+        bst_print(root->left);
+        node_print(root);
+        bst_print(root->right);
+    }
+}
