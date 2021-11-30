929cf1be9e30e9b8adc855de5f46e097391cd935
Brian Nguyen <bnguy118@ucsc.edu>
2021-11-30 06:33:37 +0000

    adding node

---
+++ asgn7/node.c
@@ -0,0 +1,40 @@
+#include "node.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Creates node array consisting of oldspeak and newspeak and NULL pointers to their children
+//
+//oldspeak: the oldspeak component of the node
+//newspeak: the newspeak component of the node
+Node *node_create(char *oldspeak, char *newspeak) {
+    Node *n = (Node *) malloc(sizeof(Node));
+    if (n) {
+        n->oldspeak = strdup(oldspeak);
+        n->newspeak = strdup(newspeak);
+        n->left = NULL;
+        n->right = NULL;
+    }
+    return n;
+}
+
+//Frees oldspeak and newspeak strdup() memory then frees the node and NULLs the pointer
+//
+//n: the node
+void node_delete(Node **n) {
+    free((*n)->oldspeak);
+    free((*n)->newspeak);
+    free(*n);
+    *n = NULL;
+}
+
+//Prints out oldspeak its newspeak translation of the node if not NULL
+//
+//n: the node
+void node_print(Node *n) {
+    if (n->newspeak) {
+        printf("%s -> %s\n", n->oldspeak, n->newspeak);
+    } else {
+        printf("%s\n", n->oldspeak);
+    }
+}
