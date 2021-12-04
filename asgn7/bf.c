d3650fb160e76682cdc63305dc8b2da23c8c8062
Brian Nguyen <bnguy118@ucsc.edu>
2021-12-04 07:42:27 +0000

    adding template bf

---
+++ asgn7/bf.c
@@ -0,0 +1,17 @@
+#include "bf.h"
+
+
+BloomFilter *bf_create(uint32_t size);
+
+void bf_delete(BloomFilter **bf);
+
+uint32_t bf_size(BloomFilter *bf);
+
+void bf_insert(BloomFilter *bf, char *oldspeak);
+
+bool bf_probe(BloomFilter *bf, char *oldspeak);
+
+uint32_t bf_count(BloomFilter *bf);
+
+void bf_print(BloomFilter *bf);
+
